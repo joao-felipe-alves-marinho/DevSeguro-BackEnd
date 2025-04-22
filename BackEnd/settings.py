1a26785337e53c415d3d330378de71c3c24725e8
Joao Felipe <joao.felipe.alves.m@gmail.com>
2025-04-21 21:37:52 -0300

    Created requirements.txt

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +127,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'BackEndApi.User'
 AUTH_PASSWORD_RESET_URL = "http://localhost:8000/reset/"
 CORS_ALLOW_ALL_ORIGINS = True
+
+AUTH_JWT_ACCESS_TOKEN_LIFETIME = timedelta(
+    minutes=int(os.getenv('AUTH_JWT_ACCESS_TOKEN_LIFETIME', '5'))
+)
+AUTH_JWT_REFRESH_TOKEN_LIFETIME = timedelta(
+    days=int(os.getenv('AUTH_JWT_REFRESH_TOKEN_LIFETIME', '1'))
+)
